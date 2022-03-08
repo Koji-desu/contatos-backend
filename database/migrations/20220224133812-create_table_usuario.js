4cada1eba82dbd3e8be0c6d8c86ee58dc1704929
Koji-Desu <pdigo153@gmail.com>
2022-03-08 10:48:37 -0300

    :memo: migrations feitas usuarios e contatos

--- database/migrations/20220224133812-create_table_usuario.js
+++ database/migrations/20220224133812-create_table_usuario.js
@@ -3,7 +3,28 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     
-      await queryInterface.createTable('usuarios', { id: Sequelize.INTEGER });
+      await queryInterface.createTable('usuarios', 
+      {
+         id:{ 
+          type: Sequelize.INTEGER,
+          autoIncrement:true,
+          primaryKey: true,
+          allowNull: false
+         },
+         email: {
+           type: Sequelize.STRING(100),
+           allowNull: false
+         },
+         nome: {
+           type: Sequelize.STRING(100),
+           allowNull: false
+         },
+         senha: {
+           type: Sequelize.STRING(256),
+           allowNull: false
+         }
+
+       });
 
   },
 
