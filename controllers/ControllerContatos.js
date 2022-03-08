b43aaa8c70715f9a292f4c0b5efc84edae6bb73a
Koji-Desu <pdigo153@gmail.com>
2022-03-08 09:57:25 -0300

    Criado o controller de contatos :memo:

--- controllers/ControllerContatos.js
+++ controllers/ControllerContatos.js
@@ -1,3 +1,29 @@
-module.exports = (req,res)=>{
-    contatos: {}
+
+
+
+module.exports = {
+    index: (req, res)=>{
+        res.send('Contatos')
+    },
+    show: (req, res)=>{
+        let {id} = req.params.id
+        res.send()
+    },
+    create: (req, res)=>{
+        let {nome, email} = req.body
+
+        let usuario = Usuario.create({
+            nome, email
+        })
+        res.send('Contatos')
+    },
+    destroy: (req, res)=>{
+        res.send('Contatos')
+    },
+    update: (req, res)=>{
+        res.send('Contatos')
+    },
+    search: (req, res)=>{
+        res.send('Contatos')
+    }
 }
--- routes/contatosRouter.js
+++ routes/contatosRouter.js
@@ -4,16 +4,17 @@ const ControllerContatos = require('../controllers/ControllerContatos')
 
 /* Listar contatos                              | GET     | /contatos
 Listar informações de um contato específico  | GET     | /contatos/:id
+Buscar um contatos                           | GET  | /contatos/search
 Cadastrar um contato                         | POST    | /contatos
 Deletar um contato                           | DELETE  | /contatos/:id
 Alterar um contato                           | PUT  | /contatos/:id
-
 */
 
-router.get('/', (req, res)=>{res.send('get contatos')})
-router.get('/:id', (req, res)=>{res.send(`get contatos ${req.params.id}`)})
-router.post('/', (req, res)=>{res.send('post contatos')})
-router.delete('/:id', (req, res)=>{res.send(`delete contato ${req.params.id}`)})
-router.put('/:id', (req, res)=>{res.send(`update contato ${req.params.id}`)})
+router.get('/', ControllerContatos.index )
+router.get('/:id', ControllerContatos.show )
+router.get('/search', ControllerContatos.search)
+router.post('/', ControllerContatos.create )
+router.delete('/:id', ControllerContatos.destroy )
+router.put('/:id', ControllerContatos.update )
 
 module.exports = router
