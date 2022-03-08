be1d778cebaafa4488870aef26e3b8e95dbbb3f8
Koji-Desu <pdigo153@gmail.com>
2022-03-08 11:19:07 -0300

    :seedling: seeders feitos

--- database/seeders/20220224141321-usuarios.js
+++ database/seeders/20220224141321-usuarios.js
@@ -1,13 +1,15 @@
 'use strict';
+const bcrypt = require('bcryptjs');
+
 
 module.exports = {
   async up (queryInterface, Sequelize) {
    
        await queryInterface.bulkInsert(
          'usuarios', [
-     {id: 1},
-     {id: 2},
-     {id: 3}
+     {id: 1, nome: 'Rafaela', email: 'rafaela@meta.com', senha: bcrypt.hashSync('123456', 10)},
+     {id: 3, nome: 'Elvis' , email: 'elvis@ibm.com', senha: bcrypt.hashSync('123456', 10)},
+     {id: 2, nome: 'Luis' , email: 'luis@google.com', senha: bcrypt.hashSync('123456', 10)},
     ], 
       {});
    
