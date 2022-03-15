46452478037d424e8bc77de65003a6e1cefc2eef
Koji-Desu <pdigo153@gmail.com>
2022-03-15 11:14:58 -0300

    Adição das migrations de telefones e emails

---
+++ database/migrations/20220315133819-create_table_telefones.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     */
+    await queryInterface.createTable(
+      'telefones', 
+      { id: 
+        {type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+        },
+        email: {type: Sequelize.STRING(45), allowNull: false},
+        contato_id: {type: Sequelize.INTEGER, 
+        allowNull: false,
+         references: 
+              {model:
+                {tableName:'contatos'},
+                 key:'id'
+                }}
+      });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     */
+    await queryInterface.dropTable('telefones');
+  }
+};
